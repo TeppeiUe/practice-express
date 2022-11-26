211f315ef30a913de482c1bcdbb0760bdd96530d
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2022-11-26 14:03:11 +0900

    [FIX] integrate if-statements for simplicity

--- node/app/filters/tweet_validator.js
+++ node/app/filters/tweet_validator.js
@@ -12,40 +12,34 @@ const { check } = require('../services');
   const user_id = req.current_user.id;
   let err_msg = [];
 
-  if (message === undefined) {
-    err_msg.push('message is undefined');
-  }
-  if (user_id === undefined) {
-    err_msg.push('user_id is undefined');
-  }
-
   try {
+    if (message === undefined) {
+        err_msg.push('message is undefined');
+    } else
+    if (check.is_empty(message)) {
+      err_msg.push('message is empty');
+    } else
+    if (!check.is_string(message)) {
+      err_msg.push('message is not string');
+    } else
+    if (!check.is_string_within_range(message, 1, 140)) {
+      err_msg.push('message is out of range');
+    }
+
+    if (user_id === undefined) {
+      err_msg.push('user_id is undefined');
+    } else
+    if (check.is_empty(user_id)) {
+      err_msg.push('user_id is empty');
+    } else
+    if (!check.is_positive_integer(user_id)) {
+      err_msg.push('user_id is not positive integer');
+    }
+
     if (err_msg.length) {
       callback.failure(err_msg);
-
     } else {
-      if (check.is_empty(message)) {
-        err_msg.push('message is empty');
-      } else
-      if (!check.is_string(message)) {
-        err_msg.push('message is not string');
-      } else
-      if (!check.is_string_within_range(message, 1, 140)) {
-        err_msg.push('message is out of range');
-      }
-
-      if (check.is_empty(user_id)) {
-        err_msg.push('user_id is empty');
-      } else
-      if (!check.is_positive_integer(user_id)) {
-        err_msg.push('user_id is not positive integer');
-      }
-
-      if (err_msg.length) {
-        callback.failure(err_msg);
-      } else {
-        callback.success({ user_id, message });
-      }
+      callback.success({ user_id, message });
     }
 
   } catch (err) {
@@ -66,28 +60,21 @@ const { check } = require('../services');
   const tweet_id = req.params.id;
   let err_msg = [];
 
-  if (tweet_id === undefined) {
-    err_msg.push('tweet_id is undefined');
-  }
-
   try {
+    if (tweet_id === undefined) {
+      err_msg.push('tweet_id is undefined');
+    } else
+    if (check.is_empty(tweet_id)) {
+      err_msg.push('tweet_id is empty');
+    } else
+    if (!check.is_positive_integer(tweet_id)) {
+      err_msg.push('tweet_id is not positive integer');
+    }
+
     if (err_msg.length) {
       callback.failure(err_msg);
-
     } else {
-      if (check.is_empty(tweet_id)) {
-        err_msg.push('tweet_id is empty');
-      } else
-      if (!check.is_positive_integer(tweet_id)) {
-        err_msg.push('tweet_id is not positive integer');
-      }
-
-      if (err_msg.length) {
-        callback.failure(err_msg);
-      } else {
-        callback.success({ id: tweet_id });
-      }
-
+      callback.success({ id: tweet_id });
     }
 
   } catch (err) {
@@ -109,37 +96,31 @@ const { check } = require('../services');
   const user_id = req.current_user.id;
   let err_msg = [];
 
-  if (tweet_id === undefined) {
-    err_msg.push('tweet_id is undefined');
-  }
-  if (user_id === undefined) {
-    err_msg.push('user_id is undefined');
-  }
-
   try {
+    if (tweet_id === undefined) {
+      err_msAg.push('tweet_id is undefined');
+    } else
+    if (check.is_empty(tweet_id)) {
+      err_msg.push('user_id is empty');
+    } else
+    if (!check.is_positive_integer(tweet_id)) {
+      err_msg.push('user_id is not positive integer');
+    }
+
+    if (user_id === undefined) {
+      err_msg.push('user_id is undefined');
+    } else
+    if (check.is_empty(user_id)) {
+      err_msg.push('user_id is empty');
+    } else
+    if (!check.is_positive_integer(user_id)) {
+      err_msg.push('user_id is not positive integer');
+    }
+
     if (err_msg.length) {
       callback.failure(err_msg);
-
     } else {
-      if (check.is_empty(tweet_id)) {
-        err_msg.push('user_id is empty');
-      } else
-      if (!check.is_positive_integer(tweet_id)) {
-        err_msg.push('user_id is not positive integer');
-      }
-
-      if (check.is_empty(user_id)) {
-        err_msg.push('user_id is empty');
-      } else
-      if (!check.is_positive_integer(user_id)) {
-        err_msg.push('user_id is not positive integer');
-      }
-
-      if (err_msg.length) {
-        callback.failure(err_msg);
-      } else {
-        callback.success({ user_id, tweet_id });
-      }
+      callback.success({ user_id, tweet_id });
     }
 
   } catch (err) {
